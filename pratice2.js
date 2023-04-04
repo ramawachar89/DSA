16a5caf8b93cdc52303d3f6eb32c05248ce820c2
ramawachar <ramawachar621@gmail.com>
2023-04-04 15:15:26 +0530

    unique pair

--- pratice2.js
+++
@@ -1,6 +0,0 @@
-//DSA 
-//1)Frequency
-//2)Two pointer method
-//3)Sliding window
-//4)recursion
-//5)recursion
---
+++ uniquePair.js
@@ -0,0 +1,24 @@
+//find unique pair in an array
+let array = [11, 22, 3, 4, 5, 6, 3, 55, 6, 11, 5, 4, 3, 2] //22,3,55,
+//freq 
+let ans=[]
+let mp = new Map()
+function uniquePair(arr) {
+    for (i = 0; i < arr.length; i++) {
+        if (mp.has(arr[i])) {
+            mp.set(arr[i], mp.get(arr[i]) + 1)
+        } else {
+            mp.set(arr[i], 1)
+        }
+       
+        mp.forEach((value, key) => {
+      
+            if (value % 2 == 1) {
+                ans.push(key)
+            }
+
+        })
+    }
+    return ans
+}
+console.log(uniquePair(array))
