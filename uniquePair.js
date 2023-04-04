ca517fe9be50f6c38f6bd27e1fd51249baddd0ee
ramawachar <ramawachar621@gmail.com>
2023-04-04 19:34:17 +0530

    missing repeated

--- uniquePair.js
+++ uniquePair.js
@@ -3,22 +3,24 @@ let array = [11, 22, 3, 4, 5, 6, 3, 55, 6, 11, 5, 4, 3, 2] //22,3,55,
 //freq 
 let ans=[]
 let mp = new Map()
+console.log(mp)
 function uniquePair(arr) {
     for (i = 0; i < arr.length; i++) {
         if (mp.has(arr[i])) {
-            mp.set(arr[i], mp.get(arr[i]) + 1)
+            mp.set(arr[i], mp.get(arr[i])+1)
         } else {
             mp.set(arr[i], 1)
         }
+        console.log(mp)
        
-        mp.forEach((value, key) => {
-      
-            if (value % 2 == 1) {
-                ans.push(key)
-            }
-
-        })
+       
+      mp.forEach((value,key)=>{
+        if(value %2==1){
+            ans.push(key)
+        }
+      })
     }
-    return ans
+   
+return ans
 }
 console.log(uniquePair(array))
