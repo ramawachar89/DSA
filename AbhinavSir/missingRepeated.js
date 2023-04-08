671ea451e20eed866eda20ac912651e0061906a4
ramawachar <ramawachar621@gmail.com>
2023-04-08 22:44:38 +0530

    subarray01

--- AbhinavSir/missingRepeated.js
+++ AbhinavSir/missingRepeated.js
@@ -1,27 +1,27 @@
-let arr=[4,3,1,2,6,2]
-let n=arr.length
+// let arr=[4,3,1,2,6,2]
+// let n=arr.length
 
-let missing,repeated
+// let missing,repeated
 
-for(let i=0;i<n;i++){
-    let x=Math.abs(arr[i])
+// for(let i=0;i<n;i++){
+//     let x=Math.abs(arr[i])
     
-    let index=x-1
-    if(arr[index]<0){
-        repeated=x
-        console.log(repeated)
-    }
+//     let index=x-1
+//     if(arr[index]<0){
+//         repeated=x
+//         console.log(repeated)
+//     }
    
-}
-    else{
-        arr[index]*=-1
-    }
-}
+// }
+//     else{
+//         arr[index]*=-1
+//     }
+// }
 
-for(let i=0;i<n;i++){
-    if(arr[i]>0){
-        missing=i+1
-    }
-}
+// for(let i=0;i<n;i++){
+//     if(arr[i]>0){
+//         missing=i+1
+//     }
+// }
 
-console.log("repeated" ,repeated," and missing ",missing);
+// console.log("repeated" ,repeated," and missing ",missing);
