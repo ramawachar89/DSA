113b282d4c55a9a87492d1d73472fd77308e432b
ramawachar <ramawachar621@gmail.com>
2023-04-13 23:51:01 +0530

    count square

---
+++ AbhinavSir/sorting/DSACODINGQUEST/placements/countsquare.js
@@ -0,0 +1,23 @@
+function countSquares(N,x) {
+
+    var l=1;
+    var r=x
+    var ans=0
+    while(l<=r){
+        var mid=Math.floor((l+r)/2)
+        if(mid*mid==x){
+            ans=mid
+            break;
+        }
+        else if(mid*mid<x){
+            ans=mid
+            l=mid+1
+        }
+        else{
+            r=mid-1
+        }
+    }
+    return ans
+};
+console.log(countSquares(1,9));
+    
