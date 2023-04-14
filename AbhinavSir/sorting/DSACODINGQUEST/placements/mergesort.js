1a197ab96cd7c9673b116ce6d95450943261edbd
ramawachar <ramawachar621@gmail.com>
2023-04-14 20:16:45 +0530

    merge sort

---
+++ AbhinavSir/sorting/DSACODINGQUEST/placements/mergesort.js
@@ -0,0 +1,34 @@
+let a=[1,3,4,5]
+let b=[3,2,1,3,4]
+
+
+var n=a.length,m=b.length
+let c=new Array(m+n)
+
+let i=0,j=0,k=0
+while(i<n&&j<m){
+    if(a[i]<b[j]){
+        c[k]=a[i]
+        i++
+        k++
+    }
+    else{
+        c[k]=b[j]
+        k++
+        j++
+    }
+}
+
+while(i<n){
+    c[k]=a[i]
+    i++
+    k++
+}
+
+while(j<m){
+    c[k]=b[j]
+    k++
+    j++
+}
+
+console.log(c);
