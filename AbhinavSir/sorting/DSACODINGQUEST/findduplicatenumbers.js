46759ae52e4dd98baafe9affa1909698ec757acb
ramawachar <ramawachar621@gmail.com>
2023-04-21 23:51:34 +0530

    changes done

--- AbhinavSir/sorting/DSACODINGQUEST/findduplicatenumbers.js
+++ AbhinavSir/sorting/DSACODINGQUEST/findduplicatenumbers.js
@@ -1,24 +1,24 @@
 // //Find all duplicate string in the string
 
 
-// let freq={}
-// let ans=[]
+let freq={}
+let ans=[]
 
-// function duplicatestring(str){
+function duplicatestring(str){
    
-//   for(let i=0;i<str.length;i++){
-//     freq[str[i]]=(freq[str[i]] || 0 )+ 1
-//     console.log(freq)
-//   }
-//   for(let key in freq){
-//     if(freq[key]>1){
-//       ans+=(key)
-//     }
+  for(let i=0;i<str.length;i++){
+    freq[str[i]]=(freq[str[i]] || 0 )+ 1
+    console.log(freq)
+  }
+  for(let key in freq){
+    if(freq[key]>1){
+      ans+=(key)
+    }
     
-//   }
-// return ans
-// }
-// console.log(duplicatestring("geekforgeeks"))
+  }
+return ans
+}
+console.log(duplicatestring("geekforgeeks"))
 
 
 
