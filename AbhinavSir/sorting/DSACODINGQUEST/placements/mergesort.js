31ae90d80c31cade85ae522be41db1ab2e4591c6
ramawachar <ramawachar621@gmail.com>
2023-04-26 23:34:17 +0530

    merge

--- AbhinavSir/sorting/DSACODINGQUEST/placements/mergesort.js
+++ AbhinavSir/sorting/DSACODINGQUEST/placements/mergesort.js
@@ -1,6 +1,40 @@
-let a=[1,3,4,5]
-let b=[3,2,1,3,4]
+// let a=[1,3,4,5]
+// let b=[3,2,1,3,4]
 
+
+// var n=a.length,m=b.length
+// let c=new Array(m+n)
+
+// let i=0,j=0,k=0
+// while(i<n&&j<m){
+//     if(a[i]<b[j]){
+//         c[k]=a[i]
+//         i++
+//         k++
+//     }
+//     else{
+//         c[k]=b[j]
+//         k++
+//         j++
+//     }
+// }
+
+// while(i<n){
+//     c[k]=a[i]
+//     i++
+//     k++
+// }
+
+// while(j<m){
+//     c[k]=b[j]
+//     k++
+//     j++
+// }
+
+// console.log(c);
+
+let a=[2,5,8,9]
+let b=[3,4,7]
 
 var n=a.length,m=b.length
 let c=new Array(m+n)
@@ -31,4 +65,4 @@ while(j<m){
     j++
 }
 
-console.log(c);
+console.log(c)
