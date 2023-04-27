65d2fc1fc24c1ec63af3a884d36745ac0e9e2877
ramawachar <ramawachar621@gmail.com>
2023-04-27 23:42:35 +0530

    subarray equal k

---
+++ AbhinavSir/sorting/DSACODINGQUEST/placements/subarraysumequalk.js
@@ -0,0 +1,13 @@
+var subarraySum = function(arr, k) {
+    let map=new Map()
+    let count=0
+    let sum=0;
+    map.set(0,1)
+    for(let i=0;i<arr.length;i++){
+        sum+=arr[i]
+        if(map.has(sum-k)) count+=map.get(sum-k)
+       
+        else map.set(sum,1)
+    }
+    return count
+};
