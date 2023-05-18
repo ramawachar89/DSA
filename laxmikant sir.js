aa1fd5aa1a16376b9fad23bd186e27f1799af40b
ramawachar <ramawachar621@gmail.com>
2023-05-18 23:41:15 +0530

    c

---
+++ laxmikant sir.js
@@ -0,0 +1,33 @@
+const firstName = "Lakshmikant";
+const lastName = "Deshpande";
+
+const assets = [
+    { course: "LLD of Splitwise", type: "video", link: "https://youtu.be/I4xf4STXgmU" },
+    { course: "LLD of Parking", type: "blog", link: "https://medium.com/@abhigulve06/parking-lot-low-level-design-in-java-2be46101daec" }
+]
+
+const videos = assets.filter(asset => asset.type === "video");
+console.log(videos)
+
+// 2. Loose Equality Operators
+
+console.log([100] == 100); // true
+console.log([100] === 100); // false
+
+console.log('100' == 100); // true
+console.log('100' === 100); // false
+
+console.log([] == 0); // true
+console.log([] === 0); // false
+
+console.log('' == false); // true
+console.log('' === false); // false
+
+console.log('' == 0); // true
+console.log('' === 0); // false
+
+console.log(0 == false); // true
+console.log(0 === false); // false
+
+console.log(null == undefined); // true
+console.log(null === undefined); // false
